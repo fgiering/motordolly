c61fd775a37f6eeadb005bb95799d97cdcb6bac6
fgiering <fgiering@web.de>
2019-06-12 22:31:19 +0200

    add libraries and vscode settings

--- motordolly.ino
+++ motordolly.ino
@@ -9,10 +9,10 @@
 #define TONE_PIN 7
 //-----Motor pins-----//
 #define HALFSTEP 8
-#define motorPin1  2
-#define motorPin2  3
-#define motorPin3  4
-#define motorPin4  5
+#define motorPin1 2
+#define motorPin2 3
+#define motorPin3 4
+#define motorPin4 5
 
 //-----LED Pins-----//
 const byte LEDr = 8;
@@ -25,33 +25,33 @@ byte nextmenustep = 0;
 byte menustep = 0; // 1= dolly{1=direction, 2=duration, 3=range}, 4=timelapse{4=shots, 5=interval, 6=distance}, 5= setup etc.
 const byte numScreens = 12;
 const String menuscreens[numScreens][2][3] = {
-  { {"Mode", ""},            {"Dolly", "Timelapse", "Setup"}   }, //root 0
-  { {"Direction", ""},       {"forwards", "backwards"}         }, //dolly 1
-  { {"Duration", "s"},       {"", ""}                          }, //dolly 2
-  { {"Distance", "cm"},      {"", ""}                          }, //dolly 3
-  { {"Shots", "shots"},      {"", ""}                          }, //timelapse 4
-  { {"Interval", "s"},       {"", ""}                          }, //timelapse 5
-  { {"Distance", "cm"},      {"", ""}                          }, //timelapse 6
-  { {"Want to start?", ""},  {"", ""}                          }, //timelapse 7
-  { {"Sound", ""},           {"On", "Off"}                     }, //setup 8
-  { {"LED", ""},             {"On", "Off"}                     }, //setup 9
-  { {"Ease In/Out", ""},     {"On", "Off"}                     }, //setup 10
-  { {"Repeat Movement", ""}, {"Yes", "No"}                     }  //repeat 11
+    {{"Mode", ""}, {"Dolly", "Timelapse", "Setup"}}, //root 0
+    {{"Direction", ""}, {"forwards", "backwards"}},  //dolly 1
+    {{"Duration", "s"}, {"", ""}},                   //dolly 2
+    {{"Distance", "cm"}, {"", ""}},                  //dolly 3
+    {{"Shots", "shots"}, {"", ""}},                  //timelapse 4
+    {{"Interval", "s"}, {"", ""}},                   //timelapse 5
+    {{"Distance", "cm"}, {"", ""}},                  //timelapse 6
+    {{"Want to start?", ""}, {"", ""}},              //timelapse 7
+    {{"Sound", ""}, {"On", "Off"}},                  //setup 8
+    {{"LED", ""}, {"On", "Off"}},                    //setup 9
+    {{"Ease In/Out", ""}, {"On", "Off"}},            //setup 10
+    {{"Repeat Movement", ""}, {"Yes", "No"}}         //repeat 11
 };
 unsigned int parameters[numScreens] = {0, 0, 3, 5, 3, 15, 5, 0, 0, 0, 0, 0};
 
 //-------Input-------//
 byte keyvalue = 0;
 boolean getnumber = false;
-byte numArray [4] = {0, 0, 0, 1}; //for 4-digit NumberInputs
+byte numArray[4] = {0, 0, 0, 1}; //for 4-digit NumberInputs
 boolean cancel = false;
 byte activeDigit = 0;
 
 //------Motor------//
 AccelStepper stepper(HALFSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
 const long stepsPerCm = 450; //dislocation of motor to wheel
-const int fullSpeed = 1000; //maxSpeed = 2cm/s
-byte movedirection = 0; //0=forwards, 1=backwards;
+const int fullSpeed = 1000;  //maxSpeed = 2cm/s
+byte movedirection = 0;      //0=forwards, 1=backwards;
 
 //-----IR-Remote-----//
 byte IRmodul = 6;
@@ -61,7 +61,6 @@ boolean getNewInput = true;
 
 //------Display------//
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 
 void setup()
 {
@@ -83,39 +82,50 @@ void setup()
   printScreen(); //Show first menustep on Startup
 }
 
-void loop() {       //Main Function
+void loop()
+{ //Main Function
   cancel = false;
   keyvalue = recieveIR(200);
-  if (keyvalue != 0 && keyvalue != 1 && !cancel) {
+  if (keyvalue != 0 && keyvalue != 1 && !cancel)
+  {
     inputAction(keyvalue);
     printScreen();
   }
   inputAction(0);
 }
 
-void printScreen() {  //Function to print text from the menuscreens Array on the Screen
+void printScreen()
+{ //Function to print text from the menuscreens Array on the Screen
   //String activeParam = menuscreens[menustep][1][parameters[menustep]];
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(menuscreens[menustep][0][0]);
   lcd.setCursor(0, 1);
   lcd.print(F("> "));
-  if (getnumber) { // if number input active
-    if (activeDigit < 4) {
-      if (keyvalue < 22 && keyvalue > 12 || keyvalue == 10) {
-        if (keyvalue == 10) {
+  if (getnumber)
+  { // if number input active
+    if (activeDigit < 4)
+    {
+      if (keyvalue < 22 && keyvalue > 12 || keyvalue == 10)
+      {
+        if (keyvalue == 10)
+        {
           numArray[activeDigit] = 0;
-        } else {
+        }
+        else
+        {
           numArray[activeDigit] = keyvalue - 12;
         }
         activeDigit++;
       }
     }
-    if (activeDigit >= 4) {
+    if (activeDigit >= 4)
+    {
       activeDigit = 0;
     }
-    if (keyvalue != 5) {
-      parameters[menustep] = numArray[0] * 1000 + numArray[1] * 100 + numArray[2] * 10 + numArray[3];  
+    if (keyvalue != 5)
+    {
+      parameters[menustep] = numArray[0] * 1000 + numArray[1] * 100 + numArray[2] * 10 + numArray[3];
     }
     if (parameters[menustep] < 1000)
       lcd.print(F("0"));
@@ -124,11 +134,14 @@ void printScreen() {  //Function to print text from the menuscreens Array on the
     if (parameters[menustep] < 10)
       lcd.print(F("0"));
     lcd.print(parameters[menustep]);
-    
+
     lcd.print(F(" "));
     lcd.print(menuscreens[menustep][0][1]); //unit
-  } else {
-    if (menustep == 7) { //screen before start timelapse
+  }
+  else
+  {
+    if (menustep == 7)
+    { //screen before start timelapse
       lcd.print(parameters[4]);
       lcd.print(F("p "));
       lcd.print(parameters[5]);
@@ -136,13 +149,15 @@ void printScreen() {  //Function to print text from the menuscreens Array on the
       lcd.print(parameters[6]);
       lcd.print(F("cm"));
     }
-    else { //screen with fixed values
+    else
+    { //screen with fixed values
       lcd.print(menuscreens[menustep][1][parameters[menustep]]);
     }
   }
 }
 
-void reset() { //Function to Reset all e.g. after canceling
+void reset()
+{ //Function to Reset all e.g. after canceling
   stepper.stop();
   delay(50);
   changeMenuStep(0);
@@ -150,12 +165,16 @@ void reset() { //Function to Reset all e.g. after canceling
   printScreen();
 }
 
-void piep(int times, int waitTime) { //function to piep the speaker with Parameters 1-how often 2-how long to wait between in ms
+void piep(int times, int waitTime)
+{ //function to piep the speaker with Parameters 1-how often 2-how long to wait between in ms
   unsigned long preMillis = 0;
   int pieped = 0;
-  if (parameters[8] == 0) {
-    while (pieped < times) {
-      if (millis() >= preMillis + waitTime) {
+  if (parameters[8] == 0)
+  {
+    while (pieped < times)
+    {
+      if (millis() >= preMillis + waitTime)
+      {
         NewTone(TONE_PIN, 2025); //pin, freq
         delay(300);
         noNewTone(TONE_PIN);
@@ -166,51 +185,65 @@ void piep(int times, int waitTime) { //function to piep the speaker with Paramet
   }
 }
 
-void changeMenuStep(byte newStep) { //function to change the MenuStep in the menu
+void changeMenuStep(byte newStep)
+{ //function to change the MenuStep in the menu
   menustep = newStep;
   getnumber = false;
   activeDigit = 0;
   lcd.noBlink();
   piep(1, 0);
-  for (byte i = 0; i < 4; ++i) {//reset numArray
+  for (byte i = 0; i < 4; ++i)
+  { //reset numArray
     numArray[i] = 0;
   }
-  switch (menustep) {
-    case 2:
-      numberInput();
-      break;
-    case 3:
-      numberInput();
-      break;
-    case 4:
-      numberInput();
-      break;
-    case 5:
-      numberInput();
-      break;
-    case 6:
-      numberInput();
-      break;
+  switch (menustep)
+  {
+  case 2:
+    numberInput();
+    break;
+  case 3:
+    numberInput();
+    break;
+  case 4:
+    numberInput();
+    break;
+  case 5:
+    numberInput();
+    break;
+  case 6:
+    numberInput();
+    break;
   }
 }
 
-void changeParameter(int values) { //function to change a Parameter in the Parameter Array with Paramter 1-how many values do i have
-  if (keyvalue == 9 && parameters[menustep] < values) {
-    if (parameters[menustep] == values - 1) {
+void changeParameter(int values)
+{ //function to change a Parameter in the Parameter Array with Paramter 1-how many values do i have
+  if (keyvalue == 9 && parameters[menustep] < values)
+  {
+    if (parameters[menustep] == values - 1)
+    {
       parameters[menustep] = 0;
-    } else {
+    }
+    else
+    {
       parameters[menustep]++;
     }
-  } else if (keyvalue == 7 && parameters[menustep] >= 0) {
-    if (parameters[menustep] == 0) {
+  }
+  else if (keyvalue == 7 && parameters[menustep] >= 0)
+  {
+    if (parameters[menustep] == 0)
+    {
       parameters[menustep] = values - 1;
-    } else {
+    }
+    else
+    {
       parameters[menustep]--;
     }
   }
 }
 
-void numberInput () { //Function to input 4-digit numbers with remote an display it on Screen
+void numberInput()
+{ //Function to input 4-digit numbers with remote an display it on Screen
   getnumber = true;
   lcd.setCursor(activeDigit + 2, 1);
   lcd.blink();
